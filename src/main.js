827b0c82728ed7e0e6c695416670b6fd341afc2f
Nobody4931 <ardenxie1@gmail.com>
2021-06-07 15:27:47 -0400

    things and stuff

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import BodyParser from "body-parser";
+import Express from "express";
+import * as Config from "../config.js";
+
+export const App = Express();
+
+/* INITIALIZATION */
+App.disable("x-powered-by");
+App.use(BodyParser.urlencoded({ extended: false }));
+App.use(BodyParser.json());
+
+/* BACKEND */
+
+/* FRONTEND */
+App.use("/", Express.static(`${process.cwd()}/src/frontend`, { index: "index.html" }));
+App.use("/files", Express.static(`${process.cwd()}/res`));
+App.get("*", (_Request, Response) => Response.redirect("/"));
+
+/* STARTUP */
+App.listen(Config.PORT, () =>
+	console.log(`Server started successfully on port ${Config.PORT}!`));
