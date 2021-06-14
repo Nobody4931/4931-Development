590e3e82dd394d04141359be38850632327771de
Nobody4931 <ardenxie1@gmail.com>
2021-06-13 21:42:01 -0400

    make this garbage slightly less garbage

--- build/css.bat
+++ build/css.bat
@@ -1,6 +1,11 @@
 @echo off
 
 for /R ..\src %%f in (*.scss) do (
-	call sass --no-source-map "%%f":"%%~dpf%%~nf.css"
-	call postcss "%%~dpf%%~nf.css" --use autoprefixer --output "%%~dpf%%~nf.css" 
+	call sass^
+		--no-source-map^
+		"%%f":"%%~dpf%%~nf.css"
+	call postcss "%%~dpf%%~nf.css"^
+		--no-map^
+		--use autoprefixer cssnano^
+		--output "%%~dpf%%~nf.css"
 )
